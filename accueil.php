8dce8bc0351f0c0996f2944c5f9ef30e0c4a4614
sekongo <sekongomoussa77@gmail.com>
2025-03-18 22:26:18 +0000

    new

--- accueil.php
+++ accueil.php
@@ -26,15 +26,22 @@
         </header>
         <div class="bod">
             <div class="pub"><p> BENEFICIEZ D'UNE REDUCTION DE 10% SUR VOTRE PREMIÈRE COMMANDE !!!</p></div>
-            <div class="small_bod">
-                <div class="little_bold1">
-                    <p> CUISINE <span id="tr">100% AFRICAINE  </span> </p>
-                </div>
-                <div class="little_bold2"><p> COMMANDER</p></div>
-                    
+            <div class="bod_1">
+                <p> CUISINE 100% AFRICAINE</p>
+            </div>
+            <div class="table1">
+                <div class="t1"><img src="Images/p6.jpeg" alt="" class="img"></div>
+                <div class="t1"><img src="Images/f.jpeg" alt="" class="img"></div>
+                <div class="t1"><img src="Images/g1.jpeg" alt="" class="img"></div>
+                <div class="t1"><img src="Images/a77.jpeg" alt="" class="img"></div>
             </div>
 
-        </div>
+            <div class="table">
+                <div class="table2">
+                    <p>COMMANDER</p>
+                </div>
+            </div>
+
         </div>
         </div>
 
