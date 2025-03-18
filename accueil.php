17b6ec1e0295df20f161e4808a1a335c151b6053
sekongo <sekongomoussa77@gmail.com>
2025-03-18 16:38:49 +0000

    new

---
+++ accueil.php
@@ -0,0 +1,42 @@
+<DOCTYPE HTML>
+<HTML>
+    <head>
+        <meta charset="UTF-8">
+        <link href="Restau.css" rel="stylesheet">
+        
+        <title> RESTAURANT ASM  </title>
+
+    </head>
+    <body>
+        
+        <div class="Body1">
+  
+        <header>
+            <div class="nav_0">
+             <p>ASM </p>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Acceuil</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Info</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="bod">
+            <div class="pub"><p> BENEFICIEZ D'UNE REDUCTION DE 10% SUR VOTRE PREMIÈRE COMMANDE !!!</p></div>
+            <div class="small_bod">
+                <div class="little_bold1">
+                    <p> CUISINE <span id="tr">100% AFRICAINE  </span> </p>
+                </div>
+                <div class="little_bold2"><p> COMMANDER</p></div>
+                    
+            </div>
+
+        </div>
+        </div>
+        </div>
+
+    </body>
+</HTML>
